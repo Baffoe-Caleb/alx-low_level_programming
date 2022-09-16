ca07e4a96a9c52bfee29cfb06404303de5c8869d
Baffoe-Caleb <baffoeackaahcaleb@gmail.com>
2022-09-16 02:28:53 +0000

    update

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,15 @@
  * @c: input number.
  * Return: 1 if digit, 0 if otherwise.
  */
+
 int _isdigit(int c)
 {
-	if (c >=0 && c<=9)
-	
+	if (c >= 48 && c <= 57)
+	{
 		return (1);
-	
-	return (0);
-
+	}
+	else
+	{
+		return (0);
+	}
 }
